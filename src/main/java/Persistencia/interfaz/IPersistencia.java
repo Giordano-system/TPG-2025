0e89214f0d7b61cbd8b6800b83aaa25eb0ee996d
GianLuDiste <gianluca.distefanopepi@gmail.com>
2025-11-06 21:09:30 -0300

    Add: Base de Datos
    Añado las clases pertenecienes a la Base de Datos

---
+++ src/main/java/Persistencia/interfaz/IPersistencia.java
@@ -0,0 +1,18 @@
+package Persistencia.interfaz;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import Modelo.Negocio.clases.Asociado;
+import Persistencia.excepciones.AsociadoExistenteException;
+import Persistencia.excepciones.AsociadoInexistenteException;
+
+public interface IPersistencia
+{
+	public void conectar() throws SQLException;
+	public void desconectar() throws SQLException;
+	public void inicializacion() throws SQLException;
+	public ArrayList<Asociado> getAsociados();
+	public void altaBD(Asociado a) throws AsociadoExistenteException;
+	public void bajaBD(Asociado a) throws AsociadoInexistenteException;
+}
